6601bbb4472637f481fc565c22757dc76925e825
nesaranaeem <hello@nesaran.com>
2022-11-09 18:02:16 +0600

    loader added

--- src/Pages/Services/Allservices.js
+++ src/Pages/Services/Allservices.js
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ServiceCard from "../../Shared/ServiceCard";
 import { Alert } from "@material-tailwind/react";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+import Loader from "../../Shared/Loader/Loader";
 const Allservices = () => {
+  const { setLoading, loading } = useContext(AuthContext);
   const [services, setServices] = useState([]);
   useEffect(() => {
+    setLoading();
     fetch(
       "https://the-wildlife-professional-photographer-server.vercel.app/all-services"
     )
       .then((res) => res.json())
-      .then((data) => setServices(data));
-  });
+      .then((data) => {
+        setServices(data);
+      });
+  }, [setLoading]);
+
   return (
     <div className="mx-auto max-w-screen-xl my-2 py-2 px-4 lg:px-8 lg:py-4">
       <Alert className="my-4">List Of Services</Alert>
       <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {services.map((service) => (
-          <ServiceCard key={service._id} service={service}></ServiceCard>
-        ))}
+        {loading ? (
+          <Loader></Loader>
+        ) : (
+          services.map((service) => (
+            <ServiceCard key={service._id} service={service}></ServiceCard>
+          ))
+        )}
       </div>
     </div>
   );
